5a2d15f4533159f32306f849963de4ac73effb04
Mantosh Kumar <mantosh.kumar@silabs.com>
2025-11-05 15:24:41 +0530

    Remove unwanted comment

--- SiliconLabsApp/ViewControllers/AWSIoT/Controller/SILAWSIoTLEDController.swift
+++ SiliconLabsApp/ViewControllers/AWSIoT/Controller/SILAWSIoTLEDController.swift
@@ -32,8 +32,6 @@ class SILAWSIoTLEDController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
